ebb6f1bb6f7fa5b6f71be3719bedcd3820ee0946
Jo�o Pedro Borges <53550197810@>
2025-09-05 11:29:44 -0300

    commit de hoje

---
+++ back/api/views.py
@@ -0,0 +1,69 @@
+from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView,RetrieveUpdateDestroyAPIView
+from .models import Autor, Editora, Livro
+from .serializers import AutorSerializer, EditoraSerializer, LivroSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
+
+
+
+@api_view(['GET', 'POST'])
+def listar_autores(request):
+    if request.method == 'GET':
+        queryset = Autor.objects.all()
+        serializer = AutorSerializer(queryset, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = AutorSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+        
+################################################     
+class AutoresView(ListCreateAPIView):
+    queryset = Autor.objects.all()
+    serializer_class = AutorSerializer
+    permission_classes = [IsAuthenticated]
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filterset_field = ["id"]
+    search_filter = ["autor"]
+class AutoresDetailView(RetrieveUpdateDestroyAPIView):
+    queryset = Autor.objects.all()
+    serializer_class = AutorSerializer
+    permission_classes = [IsAuthenticated]
+################################################
+class EditorasView(ListCreateAPIView):
+    queryset = Editora.objects.all()
+    serializer_class = EditoraSerializer
+    permission_classes = [IsAuthenticated]
+
+class EditorasDetailView(RetrieveUpdateDestroyAPIView):
+    queryset = Editora.objects.all()
+    serializer_class = EditoraSerializer
+    permission_classes = [IsAuthenticated]
+
+################################################
+
+################################################
+class LivrosView(ListCreateAPIView):
+    queryset = Livro.objects.all()
+    serializer_class = LivroSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class LivrosDetailView(RetrieveUpdateDestroyAPIView):
+    queryset = Livro.objects.all()
+    serializer_class = LivroSerializer
+    permission_classes = [IsAuthenticated]
+
+################################################
+
+
+
+
